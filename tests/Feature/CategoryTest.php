cdc36d09b2f489d246dba84b388a098f298ffbf7
Vicky AS <vickyhuang808@gmail.com>
2023-08-01 12:32:18 +0700

    insert many

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
+use function PHPUnit\Framework\assertEquals;
 use function PHPUnit\Framework\assertTrue;
 
 class CategoryTest extends TestCase
@@ -19,5 +20,27 @@ class CategoryTest extends TestCase
         $res = $category->save();
 
         assertTrue($res);
+    }
+
+    public function testManyInsert()
+    {
+        $categories = [];
+
+        for ($i=0; $i < 10; $i++) { 
+            $categories[] = [
+                "id" => "ID $i",
+                "name" => "Name $i"
+            ];
+        }
+
+        // $res = Category::query()->insert($categories);
+        $res = Category::insert($categories);
+
+        self::assertTrue($res);
+
+        $total = Category::count();
+
+        assertEquals(10, $total);
+
     }
 }
