402ed10bc2b3675acc3e235ae7e086a03418d21d
Vicky AS <vickyhuang808@gmail.com>
2023-08-04 19:59:18 +0700

    global scope

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Category;
+use App\Models\Scopes\IsActiveScope;
 use Database\Seeders\CategorySeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -177,5 +178,18 @@ class CategoryTest extends TestCase
         $category->save();
 
         self::assertNotNull($category->id);
+    }
+    public function testGlobalScope(){
+        $category = new Category();
+        $category->id = "ANIME";
+        $category->name = "Anime";
+        $category->description = "Anime Category";
+        $category->is_active = false;
+        $category->save();
+
+        $category = Category::query()->find("ANIME");
+        self::assertNull($category);
+        $category = Category::query()->withoutGlobalScope(IsActiveScope::class)->find("ANIME");
+        self::assertNotNull($category);
     }
 }
