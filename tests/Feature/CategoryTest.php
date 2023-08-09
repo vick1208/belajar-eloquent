4e96a29568d6c9f3be252e5d10985f7fd0eda25b
Vicky AS <vickyhuang808@gmail.com>
2023-08-09 15:11:26 +0700

    query and aggregate relation

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -253,4 +253,27 @@ class CategoryTest extends TestCase
         self::assertNotNull($reviews);
         self::assertCount(2, $reviews);
     }
+
+    public function testQueryingRelation()
+    {
+        $this->seed([CategorySeeder::class, ProductSeeder::class]);
+
+        $category = Category::query()->find("FOOD");
+        $products = $category->products()->where("price", "=", "200")->get();
+
+        self::assertCount(1, $products);
+        self::assertEquals("2", $products[0]->id);
+    }
+    public function testAggregateRelation()
+    {
+        $this->seed([CategorySeeder::class, ProductSeeder::class]);
+
+        $category = Category::query()->find("FOOD");
+
+        $total = $category->products()->count();
+        self::assertEquals(2, $total);
+        $total = $category->products()->where("price", "200")->count();
+        self::assertEquals(1, $total);
+
+    }
 }
